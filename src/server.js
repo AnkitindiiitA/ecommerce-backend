2175980eafd99e63420655647af03d1a361291f8
AnkitindiiitA <ankitsharma.ind.2001@gmail.com>
2024-07-04 12:03:54 +0530

    env and server file modification

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
+require('dotenv').config();
 const { app } = require(".");
 const { connectDb } = require("./config/db");
 
-const PORT=8454;
+const PORT=process.env.PORT
 app.listen(PORT,async ()=>{
     await connectDb()
     console.log("ecommerce api listing on port ",PORT)
